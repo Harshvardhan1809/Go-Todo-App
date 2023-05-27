1833091b346f334143d6e7c8d84f51483f2f9416
Harshvardhan1809 <harrygarry2002@gmail.com>
2023-05-27 22:48:23 +0900

    made navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter } from 'react-router-dom'
+import Navbar  from "./components/Navbar";
+import { Typography } from "@mui/material";
 
 const App = () => {
-    return <div>Hello World</div>
+    return (
+            <div className="app">
+                <BrowserRouter>
+                    <Fragment>
+                        <Navbar/>
+                        <Typography variant="h2">Hello World</Typography>
+                    </Fragment>
+                </BrowserRouter>
+            </div>
+    )
 }
 
 export default App;
+
+// Navbar, hero area (with a background design and the todo section) 

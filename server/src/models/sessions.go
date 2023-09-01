d8bf78042c168b57d58625574c4953e53ac54955
Harshvardhan1809 <harrygarry2002@gmail.com>
2023-09-02 05:34:45 +0900

    commit

--- server/src/models/sessions.go
+++ server/src/models/sessions.go
@@ -1,35 +1,37 @@
 package models
 
 import (
-	"encoding/gob"
-	"github.com/gorilla/sessions"
-	"github.com/srinathgs/mysqlstore"
-	"github.com/Harshvardhan1809/Go-Todo-App/config"
+	_ "encoding/gob"
+	_ "github.com/gorilla/sessions"
+	_ "github.com/srinathgs/mysqlstore"
+	_ "github.com/Harshvardhan1809/Go-Todo-App/config"
+	_ "fmt"
 )
 
-var (
-	store *mysqlstore.MySQLStore
-	session_name = "user_session"
- 	session *sessions.Session
-)
+// var (
+// 	store *mysqlstore.MySQLStore
+// 	session_name = "user_session"
+//  	session *sessions.Session
+// )
 
-func init(){
+// func init(){
 
-	config.ConnectSessionStore()
-	store := config.GetSessionStore();
+// 	fmt.Println("Print from sessions.go file")
 
-	// Include the user struct in the session
-	gob.Register(&User{})
+// 	// config.ConnectSessionStore()
+// 	// store := config.GetSessionStore();
 
-
-	session = sessions.NewSession(store, session_name)
-	store.Options = &sessions.Options{
-		Domain: "192.168.102.222",
-		Path: "/",
-		MaxAge:     0,
-		Secure:     false,
-		HttpOnly:   true,
-	}
+// 	// // Include the user struct in the session
+// 	// gob.Register(&User{})
 
 
-}
+// 	// session = sessions.NewSession(store, session_name)
+// 	// store.Options = &sessions.Options{
+// 	// 	Domain: "192.168.102.222",
+// 	// 	Path: "/",
+// 	// 	MaxAge:     60*60*60*2, // age is given in seconds
+// 	// 	Secure:     false,
+// 	// 	HttpOnly:   true,
+// 	// }
+
+// }

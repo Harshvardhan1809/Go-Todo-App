1fcd4f31b7755727e9bb2f84a17848d480f5ce66
Harshvardhan1809 <harrygarry2002@gmail.com>
2023-09-02 03:19:46 +0900

    login and signin controllers work

--- server/src/controllers/auth_controllers.go
+++ server/src/controllers/auth_controllers.go
@@ -6,14 +6,12 @@ import (
 	"time"
 	"log"
 	"io"
-	"bytes"
 	_ "os"
 	"golang.org/x/crypto/bcrypt"
 	_ "github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/sessions"
 	"github.com/srinathgs/mysqlstore"
 	"github.com/Harshvardhan1809/Go-Todo-App/models"
-	"github.com/Harshvardhan1809/Go-Todo-App/utils"
 	"github.com/Harshvardhan1809/Go-Todo-App/config"
 	"net/http"
 )
@@ -26,8 +24,6 @@ var (
 func CheckSession(w http.ResponseWriter, r *http.Request){
 
 	fmt.Println("Print from the controller", w);
-
-	// utils.EnableCors(&w)
 
 	store = config.GetSessionStore();
 	session, err := store.Get(r, "session-name")
@@ -55,42 +51,37 @@ func CheckSession(w http.ResponseWriter, r *http.Request){
 
 func Signup(w http.ResponseWriter, r *http.Request){
 
-	utils.EnableCors(&w)
-	fmt.Println("Print request body in signup ", r.Body)
-	fmt.Println(" ")
-
-	// Get the form data 
+	// STRUCT FOR THE FORM BODY
 	var formBody struct {
-		Username string
-		Password string
+		Username string `json:"username,omitempty"`
+		Password string `json:"password,omitempty"`
 	}
-	utils.ParseBody(r, formBody)
-	
-	var newUser models.User
-	var tasks []models.Task 
-	newUser.Username = formBody.Username
-	newUser.CreatedAt = time.Now()
 
-	fmt.Println("Print the form body ", formBody)
-	fmt.Println("Credentials ", newUser.Username, formBody.Password)
+ 	// CONVERSION
+	// io.ReadCloser -> byte arrray -> string -> struct
+	bufOfRequestBody, _ := io.ReadAll(r.Body)
+	stringRequestBody := string(bufOfRequestBody)
+	json.Unmarshal([]byte(stringRequestBody), &formBody)
 
-	// Hash the password 
+	// HASH THE PASSWORD AND ASSIGN TO NEW USER
 	hash, err := bcrypt.GenerateFromPassword([]byte(formBody.Password), 10)
 	if err != nil {
 		log.Fatal("Error hashing the password")
 	}
-	newUser.Password = string(hash)
+
+	// CREATE A NEW USER
+	var newUser models.User
+	var tasks []models.Task 
+	newUser.Username = formBody.Username
+	newUser.CreatedAt = time.Now()
 	newUser.Tasks = tasks
+	newUser.Password = string(hash)
 
-	fmt.Println("Credentials after hashing", newUser.Username, newUser.Password)
-
-	// Create the user
+	// CREATE THE USER
 	u, _ := newUser.CreateUser()
 	res, _ := json.Marshal(u)
 
-	fmt.Println("user ", u, res);
-
-	// Respond
+	// WRITE TO RESPONSE
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
@@ -98,41 +89,32 @@ func Signup(w http.ResponseWriter, r *http.Request){
 
 func Login(w http.ResponseWriter, r *http.Request){
 
-	fmt.Println("In the auth controller")
-
-	// Get the email and password from the request body
+	// STRUCT FOR THE FORM BODY
 	var formBody struct {
 		Username string `json:"username,omitempty"`
 		Password string `json:"password,omitempty"`
 	}
-	// fmt.Println("Print the request in the controller", r.Body, formBody)
-	// utils.ParseBody(r, formBody)
 
+	// CONVERSION
+	// io.ReadCloser -> byte arrray -> string -> struct
 	bufOfRequestBody, _ := io.ReadAll(r.Body)
-	fmt.Println("print the request body ", bufOfRequestBody);
-	// [For Request Body] 消費されてしまったRequest Bodyを修復する
-	r.Body = io.NopCloser(bytes.NewBuffer(bufOfRequestBody))
+	stringRequestBody := string(bufOfRequestBody)
+	json.Unmarshal([]byte(stringRequestBody), &formBody)
 
-	fmt.Println("bufOfRequestBody : ", bytes.NewBuffer(bufOfRequestBody))
-	fmt.Println("r.Body : ", r.Body)
-
-	// Hash password
-	hash, err := bcrypt.GenerateFromPassword([]byte(formBody.Password), 10)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)	
+	// SEARCH IN DB 
+	var user models.User
+	db := config.GetDB() //  , 
+	qError := db.Where("username=?",formBody.Username).Find(&user)
+	if qError.Error != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	formBody.Password = string(hash)
-	// [For Request Body] 消費されてしまったRequest Bodyを修復する
-	r.Body = io.NopCloser(bytes.NewBuffer(bufOfRequestBody))
-	fmt.Println("Print the password ", formBody.Password)
-
-	// Search in DB
-	var user models.User
-	db := config.GetDB() //  , 
-	qError := db.Where("username=?",formBody.Username).Where("password=?",formBody.Password).Find(&user)
-	if qError.Error != nil {
+	// COMPARE INPUT PASSWORD WITH HASHED PASSWORD
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(formBody.Password))
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
@@ -144,9 +126,9 @@ func Login(w http.ResponseWriter, r *http.Request){
 	// session.Values["user"] = user
 	// session.Save(r, w)
 
+	// WRITE TO RESPONSE
+	w.Header().Set("Content-Type", "application/json")
 	res, _ := json.Marshal(user)
-
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
-
--- server/src/utils/env.go
+++ server/src/utils/env.go
@@ -7,7 +7,7 @@ import (
 
 func LoadEnvVariables(){
 
-	err := godotenv.Load()
+	err := godotenv.Load("/Users/harshvardhankedare/Desktop/Coding/golang/todoapp/server/src/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
